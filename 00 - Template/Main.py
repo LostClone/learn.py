cde8625379da0c7d1182f0b674878095a087fb70
LostClone <64134813+LostClone@users.noreply.github.com>
2023-02-19 15:20:12 +0700

    Add files via upload
    Kelas Terbuka Part 03

--- 00 - Template/Main.py
+++ 00 - Template/Main.py
@@ -1,1 +1,20 @@
+import time
+start_time = time.time()
+a = 10
+
+print("Hello")
 print("Hello World")
+print(a)
+
+for i in range(1,1000):
+    a = 10
+# Command End
+"""
+Multi Line Command
+Halo
+apa kabar
+"""
+# compile python dengan bytecode caranya:
+# pyhton3 -m py_compile (nama python filenya)
+
+print(time.time() - start_time)
