42476d244491f535b792df2a4cfb92fa49c810f5
LostClone <64134813+LostClone@users.noreply.github.com>
2023-03-03 16:53:50 +0700

    Add files via upload

--- 00 - Template/Main.py
+++ 00 - Template/Main.py
@@ -1,31 +1,57 @@
-#int = non decimal
+#casting = changing data types
 
-data_integer = 1
-print("Data :", data_integer)
-print("Data Bertipe :", type(data_integer))
+#integer
+print("===int===")
+data_int = 1;
+print("Data :", data_int)
+print("Tipe Data", type(data_int))
 
-#float = decimal
+data_float = float(data_int)
+data_str = str(data_int)
+data_bool = bool(data_int) # false if 0 else true
 
-data_float = 32.10
+print("Data =", data_float, ", Tipe Data =", type(data_float))
+print("Data =", data_str, ", Tipe Data =", type(data_str))
+print("Data =", data_bool, ", Tipe Data =", type(data_bool))
+
+#float
+print("===float===")
+data_float = 8.9;
 print("Data :", data_float)
-print("Data Bertipe :", type(data_float))
+print("Tipe Data", type(data_float))
 
-#string = kumpulan karakter
+data_int = int(data_float) # rounded down
+data_str = str(data_float)
+data_bool = bool(data_float) # false if 0 else true
 
-data_string = "halo 3"
-print("Data :", data_string)
-print("Data Bertipe :", type(data_string))
+print("Data =", data_int, ", Tipe Data =", type(data_int))
+print("Data =", data_str, ", Tipe Data =", type(data_str))
+print("Data =", data_bool, ", Tipe Data =", type(data_bool))
 
-#boolean = true or false
-
-data_bool = True
+#Boolean
+print("===bool===")
+data_bool = 1
 print("Data :", data_bool)
-print("Data Bertipe :", type(data_bool))
+print("Tipe Data", type(data_bool))
 
-#tipe data khusus
-#bilangan kompleks
+data_float = float(data_bool)
+data_str = str(data_bool)
+data_int = str(data_bool) # false if 0 else true
 
-data_complex = complex(4,6)
-print("Data :", data_complex)
-print("Data Bertipe :", type(data_complex))
+print("Data =", data_float, ", Tipe Data =", type(data_float))
+print("Data =", data_str, ", Tipe Data =", type(data_str))
+print("Data =", data_bool, ", Tipe Data =", type(data_bool))
 
+#string
+print("===str===")
+data_str = "2" #str must be number to be changed
+print("Data :", data_str)
+print("Tipe Data", type(data_str))
+
+data_float = float(data_str)
+data_int = int(data_str)
+data_bool = bool(data_str) # if string is filled true else false
+
+print("Data =", data_float, ", Tipe Data =", type(data_float))
+print("Data =", data_str, ", Tipe Data =", type(data_str))
+print("Data =", data_bool, ", Tipe Data =", type(data_bool))
