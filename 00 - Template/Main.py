cf55caf00c92d6423e337a61765ab2ba601c7f4c
LostClone <64134813+LostClone@users.noreply.github.com>
2023-03-03 22:48:08 +0700

    Add files via upload

--- 00 - Template/Main.py
+++ 00 - Template/Main.py
@@ -1,57 +1,15 @@
-#casting = changing data types
 
-#integer
-print("===int===")
-data_int = 1;
-print("Data :", data_int)
-print("Tipe Data", type(data_int))
 
-data_float = float(data_int)
-data_str = str(data_int)
-data_bool = bool(data_int) # false if 0 else true
+Nama = str(input("Masukkan Nama: "))
 
-print("Data =", data_float, ", Tipe Data =", type(data_float))
-print("Data =", data_str, ", Tipe Data =", type(data_str))
-print("Data =", data_bool, ", Tipe Data =", type(data_bool))
+print("data =", Nama, "Type =", type(Nama))
 
-#float
-print("===float===")
-data_float = 8.9;
-print("Data :", data_float)
-print("Tipe Data", type(data_float))
 
-data_int = int(data_float) # rounded down
-data_str = str(data_float)
-data_bool = bool(data_float) # false if 0 else true
+Umur = int(input("Masukkan Umur: "))
 
-print("Data =", data_int, ", Tipe Data =", type(data_int))
-print("Data =", data_str, ", Tipe Data =", type(data_str))
-print("Data =", data_bool, ", Tipe Data =", type(data_bool))
+print("data =", Umur, "Type =", type(Umur))
 
-#Boolean
-print("===bool===")
-data_bool = 1
-print("Data :", data_bool)
-print("Tipe Data", type(data_bool))
 
-data_float = float(data_bool)
-data_str = str(data_bool)
-data_int = str(data_bool) # false if 0 else true
+data_bool = bool(int(input("Masukkan Nilai boolean: ")))
 
-print("Data =", data_float, ", Tipe Data =", type(data_float))
-print("Data =", data_str, ", Tipe Data =", type(data_str))
-print("Data =", data_bool, ", Tipe Data =", type(data_bool))
-
-#string
-print("===str===")
-data_str = "2" #str must be number to be changed
-print("Data :", data_str)
-print("Tipe Data", type(data_str))
-
-data_float = float(data_str)
-data_int = int(data_str)
-data_bool = bool(data_str) # if string is filled true else false
-
-print("Data =", data_float, ", Tipe Data =", type(data_float))
-print("Data =", data_str, ", Tipe Data =", type(data_str))
-print("Data =", data_bool, ", Tipe Data =", type(data_bool))
+print("data =", data_bool, "Type =", type(data_bool))
